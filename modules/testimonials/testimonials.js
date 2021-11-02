e51a330524610953d9f75da46a0c6d420d52e070
kula124 <kula124@ggmail.com>
2021-11-02 20:40:54 +0100

    moving to const

--- modules/testimonials/testimonials.js
+++ modules/testimonials/testimonials.js
@@ -1,23 +1,5 @@
 import Testimonial from '../../components/testimonial';
-
-const testimonials = [
-    {
-        caption: 'New design system',
-        imageSrc: '/design-system.jpg',
-    },
-    {
-        caption: 'Design From scratch',
-        imageSrc: '/from-scratch.jpg',
-    },
-    {
-        caption: 'Brand Transformation',
-        imageSrc: '/brand.png',
-    },
-    {
-        caption: 'Book Cover Design',
-        imageSrc: '/book-cover.jpg',
-    },
-];
+import { testimonials } from '../../const/testimonialsList';
 
 const Testimonials = () => {
     return (
